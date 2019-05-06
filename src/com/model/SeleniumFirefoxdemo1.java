858b576b2995378d4bffce45d49763649c8b1a1a
IRG <akashband7@gmail.com>
2019-05-06 15:28:43 +0530

    update file

--- src/com/model/SeleniumFirefoxdemo1.java
+++ src/com/model/SeleniumFirefoxdemo1.java
@@ -7,7 +7,7 @@ public class SeleniumFirefoxdemo1 {
 
 	public static void main(String[] args) {
 		
-
+		System.out.println("The selenium  firefox demo is....");
 	 System.setProperty("webdriver.gecko.driver","E:\\Selenium\\geckodriver-v0.24.0-win64\\geckodriver.exe");
 	 WebDriver driver = new FirefoxDriver();
 	 driver.get("https://www.google.com");
